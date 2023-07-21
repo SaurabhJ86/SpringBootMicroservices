1f0690cd6c52dbc0cafe5039dfc8bb721a218e68
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2023-07-21 14:06:41 +0530

    Created Class Level JavaDoc Documentation for Plant Controller Class.

---
+++ src/main/java/com/jhingan/SpringBootMicroservices/controller/PlantDairyController.java
@@ -0,0 +1,70 @@
+package com.jhingan.SpringBootMicroservices.controller;
+
+import com.jhingan.SpringBootMicroservices.dto.Specimen;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * The controller for Plant Diary REST Endpoints
+ * <p>
+ *     This class handles the CRUD Operations for the Plant Diary Specimens via HTTP Actions
+ * </p>
+ * @author Saurabh Jhingan
+ * @version 1.0.0-SNAPSHOT
+ */
+
+@RestController
+public class PlantDairyController {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "start";
+    }
+
+    @RequestMapping("/lookup/{id}")
+    public ResponseEntity index(@PathVariable("id") String id)
+    {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.TEXT_PLAIN);
+        ResponseEntity responseEntity = new ResponseEntity("Your number: " + id, httpHeaders, HttpStatus.FORBIDDEN);
+        return responseEntity;
+    }
+
+    @GetMapping("/specimen")
+    public ResponseEntity fetchAllSpecimen()
+    {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/specimen/id/")
+    public ResponseEntity fetchSpecimenById(@PathVariable("id") String id)
+    {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /**
+     * Create a new specimen object, given the data provided.
+     *
+     * Returns one of the following codes:
+     * 201: Successfully created a new specimen.
+     * 409: Unable to create specimen, because it already exists.
+     *
+     * @param specimen a JSON representation of a specimen object.
+     * @return Newly created Specimen Object.
+     */
+    @PostMapping(value = "/specimen", consumes = "application/json", produces = "application/json")
+    public Specimen createSpecimen(@RequestBody Specimen specimen)
+    {
+        return specimen;
+    }
+
+    @DeleteMapping("/specimen/id")
+    public ResponseEntity deleteSpecimen(@PathVariable("id") String id)
+    {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
